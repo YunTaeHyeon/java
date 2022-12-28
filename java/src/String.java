6208f36a8c400e7159215a0b6815dfd55c05356a
yun <t05h19@naver.com>
2022-12-29 00:23:06 +0900

    javaString

---
+++ java/src/String.java
@@ -0,0 +1,11 @@
+public class String {
+    public static void main(java.lang.String[] args) {
+        System.out.println(12+34);
+        System.out.println("Yun"+"TaeHyeon");
+        System.out.println("Yun"+1224);
+        System.out.println(1224+"Yun");
+        System.out.println("Yun"+1224+1234);
+        System.out.println(1224+1234+"Yun");
+        System.out.println(""+12+24+"Yun");
+    }
+}
